877deaa133221c8f7b209654d6d64f620e87d819
edsoncelio <edsoncelio@users.noreply.github.com>
2021-09-11 17:23:02 -0300

    feat: add custom cases

--- tests/defaults/test_defaults.tf
+++ tests/defaults/test_defaults.tf
@@ -3,34 +3,20 @@ terraform {
     test = {
       source = "terraform.io/builtin/test"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
   }
 }
 
-resource "random_pet" "name" {}
 
 locals {
-  bucket_name = random_pet.name.id
   region      = "us-east-1"
 }
 
 module "main" {
   source = "../.."
-  bucket_name = random_pet.name.id
 }
 
 resource "test_assertions" "bucket_checks" {
   component = "bucket"
-
-  equal "bucket_name" {
-    description = "bucket name"
-    got         = module.main.bucket_name
-    want        = local.bucket_name
-  }
 
   equal "bucket_region" {
     description = "bucket region"
